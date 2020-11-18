dd90bf3e5834109104e9a9fde67382de4c5df184
Giranda22-Git <61464174+Giranda22-Git@users.noreply.github.com>
2020-11-18 21:33:10 +0600

    up

--- front/src/components/aboutUs.vue
+++ front/src/components/aboutUs.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="wrapper">
     <div class="background3D">
-О Нас
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo veritatis natus non repudiandae, optio alias voluptate accusamus. Veniam earum, dolorum ipsum iure qui nulla molestias esse velit similique illo dolores.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo aspernatur pariatur quasi magnam dolores, et doloribus dolorem fuga asperiores repellendus facilis ipsam quidem minima architecto laudantium tempore ipsum nesciunt quod.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nesciunt molestias nisi facere eius deserunt rerum doloremque expedita tempore laboriosam? Ducimus fugiat possimus deleniti dolorum. Beatae quia adipisci odit enim nostrum at voluptatem, fugiat minus eaque vel omnis quaerat tempore?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nobis libero illo repellat. Quidem doloribus fugit error ut quaerat quam repudiandae nam ad officiis enim maiores, eveniet laboriosam sapiente cum ex quis numquam expedita autem, magni eum saepe molestiae! Sit expedita porro placeat laborum? Dolor odio aperiam delectus veniam! Consequatur est earum obcaecati id voluptatem error dicta laudantium aperiam ab cum temporibus nihil deserunt at deleniti, reiciendis illum maxime iste vel dolor fugit tenetur optio odit? Rem nesciunt quaerat, suscipit possimus nulla fugit, necessitatibus autem sunt ducimus quos reprehenderit veritatis maxime laborum harum explicabo dicta laudantium, quam ipsum earum ut.
     </div>
   </div>
 </template>
@@ -19,12 +22,17 @@ export default {
     perspective: 500px
     transform-style: preserve-3d
     color: white
-    font-size: 4rem
+    font-size: 3.5rem
     .background3D
       width: 64%
       height: 100%
       margin: 0 auto
-      border: 1px solid rgba(255, 255, 255, 0.5)
+      border: 1px solid rgba(255, 255, 255, 0.2)
       box-shadow: 0 0 500px 15px rgba(60, 40, 150, 0.6)
       transform: rotateX(20deg) translateY(-10.5%)
+      overflow-y: auto
+      padding: 2%
+      text-align: justify
+      &::-webkit-scrollbar
+        width: 0rem
 </style>

a18c3e7addbd4d42e3c67c9a209c1eafbf28e861
Giranda22-Git <aloxaeee@bk.ru>
2020-11-23 18:17:37 +0600

    new

--- front/src/components/aboutUs.vue
+++ front/src/components/aboutUs.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="wrapper">
     <div class="background3D">
-О Нас
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur similique aliquam vero voluptatum vitae laborum ipsum accusamus perspiciatis odio repellat? Dolorem totam autem laboriosam ipsa suscipit, omnis sint pariatur ex?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum dolorum ipsum officiis deleniti nam corporis dolores cumque illum tenetur modi. Ducimus aperiam aut explicabo quisquam iste, voluptates eius inventore vero.
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic earum ex sequi eaque, eos expedita dicta alias autem, debitis nihil, dolorum commodi aperiam. Quo impedit quidem consequuntur earum sint consectetur.
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptas veniam quod aliquam beatae, placeat consequatur ipsum impedit numquam quo reprehenderit nisi ipsam necessitatibus? Delectus cupiditate temporibus nemo asperiores sint.
     </div>
   </div>
 </template>
@@ -27,4 +30,7 @@ export default {
       border: 1px solid rgba(255, 255, 255, 0.5)
       box-shadow: 0 0 500px 15px rgba(60, 40, 150, 0.6)
       transform: rotateX(20deg) translateY(-10.5%)
+      padding: 50px
+      text-align: justify
+      overflow-y: auto
 </style>
